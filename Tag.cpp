4ae04ef91bd83c27b64c125b78f5f80750422ce0
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-21 13:51:45 +0530

    feat: add encode tag

--- Tag.cpp
+++ Tag.cpp
@@ -7,3 +7,10 @@ enum class wiretype : uint8_t {
     Fixed64 = 2,
     Delimited = 3
 };
+
+void encodetag(std::vector<uint8_t> &buffer, uint32_t fieldnumber,
+               wiretype wiretype) {
+    uint64_t val = (static_cast<uint64_t>(fieldnumber) << 2);
+    val |= static_cast<uint64_t>(wiretype);
+    encodeVariant(buffer, val);
+}
