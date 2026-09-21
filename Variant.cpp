a3576d7aeb2113073a25347f2106b9fee0169960
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-21 13:29:04 +0530

    feat: add variant decoding

--- Variant.cpp
+++ Variant.cpp
@@ -13,3 +13,18 @@ void encodeVariant(std::vector<uint8_t> &buffer, uint64_t value) {
     buffer.push_back(lower);
 }
 
+uint64_t decodeVariant(const std::vector<uint8_t> &buffer, size_t &offset) {
+    uint64_t result = 0;
+    int shift = 0;
+    while ((buffer[offset] & 128) != 0 && offset < buffer.size()) {
+        uint8_t byte = buffer[offset++];
+        byte &= 127;
+        const uint64_t cast = static_cast<uint64_t>(byte) << shift;
+        result |= cast;
+        shift += 7;
+    }
+    uint8_t byte = buffer[offset++] & 127;
+    const uint64_t cast = static_cast<uint64_t>(byte) << shift;
+    result |= cast;
+    return result;
+}
