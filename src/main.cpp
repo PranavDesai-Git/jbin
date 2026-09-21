78a4043c3c91163afe2fe8cea367cae5aab60c7f
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-21 22:50:02 +0530

    test: parse test

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,31 @@
-#include "Tag.hpp"
-#include "Variant.hpp"
-#include <cstddef>
-#include <cstdint>
+#include "Lexer.hpp"
+#include "Parser.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
 
 int main() {
-    size_t offset = 0;
-    std::vector<uint8_t> buffer;
-    encodeString(buffer, 1, "Pranav");
-    encodeTag(buffer, 2, wiretype::Varint);
-    encodeVariant(buffer, 100);
+    std::string schemaText = R"(
+            enum Activity:
+                1. active
+                2. inactive
+            end
 
-    while (offset < buffer.size()) {
-        uint32_t fieldNumber;
-        wiretype type;
-        decodeTag(buffer, offset, fieldNumber, type);
-        switch (type) {
-        case wiretype::Varint: {
-            uint64_t val = decodeVariant(buffer, offset);
-            std::cout << "Read Field " << fieldNumber << " (Varint): " << val
-                      << std::endl;
-            break;
-        }
-        case wiretype::Delimited: {
-            std::string text = decodeString(buffer, offset);
-            std::cout << "Read Field " << fieldNumber << " (String): " << text
-                      << std::endl;
-            break;
-        }
-        default:
-            std::cerr << "Unknown wire type!" << std::endl;
-            return 1;
-        }
+            message Player:
+                1. name: string
+            end
+        )";
+
+    try {
+        std::vector<Token> tokens = tokenize(schemaText);
+
+        Parser parser(tokens);
+        Schema schema = parser.parse();
+
+        std::cout << "Successfully parsed schema" << std::endl;
+        std::cout << "Found " << schema.messages.size() << " messages and "
+                  << schema.enums.size() << " enums." << std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "Compiler Error: " << e.what() << std::endl;
     }
 
     return 0;
