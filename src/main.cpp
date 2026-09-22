99b082e713d7986b26915842ea1b5380eea85579
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-23 00:00:38 +0530

    test: add integration test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include "DynamicPacker.hpp"
 #include "Lexer.hpp"
 #include "Parser.hpp"
 #include "SemanticAnalyzer.hpp"
+#include <iomanip>
 #include <iostream>
 
 int main() {
-    // I am intentionally writing a broken schema to test the Semantic Analyzer!
-    // Can you spot the two errors?
     std::string schemaText = R"(
         // This is a test schema!
         package "com.game.core"
@@ -20,6 +20,9 @@ int main() {
             1. name: string
             2. health: i32 = 100
             3. activeStatus: Activity
+            4. weapons: list(string)
+            5. connections: list(Player)
+            6. inventory: map(string, i32)
         end
     )";
 
@@ -35,9 +38,37 @@ int main() {
         std::cout << "Successfully parsed schema" << std::endl;
         std::cout << "Package: " << schema.packageName << std::endl;
         std::cout << "Imports: ";
-        for (const auto& imp : schema.imports) std::cout << imp << " ";
+        for (const auto &imp : schema.imports)
+            std::cout << imp << " ";
         std::cout << "\nFound " << schema.messages.size() << " messages and "
                   << schema.enums.size() << " enums." << std::endl;
+
+        nlohmann::json testJson = {
+            {"name", "Hero"},
+            {"health", 100},
+            {"activeStatus", "active"},
+            {"weapons", {"Sword", "Shield"}},
+            {"connections", {
+                {{"name", "NPC1"}, {"health", 50}}
+            }},
+            {"inventory", {
+                {"gold", 500},
+                {"potions", 3}
+            }}
+        }; DynamicPacker packer(schema);
+        std::vector<uint8_t> binaryBuffer = packer.pack("Player", testJson);
+
+        std::cout << "\nPacked into " << binaryBuffer.size() << " bytes: ";
+        for (uint8_t b : binaryBuffer) {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)b
+                      << " ";
+        }
+        std::cout << std::dec << "\n";
+
+        DynamicReader reader(schema);
+        nlohmann::json unpackedJson = reader.unpack("Player", binaryBuffer);
+
+        std::cout << "Unpacked JSON: \n" << unpackedJson.dump(4) << "\n";
 
     } catch (const std::exception &e) {
         std::cerr << "Compiler Error: " << e.what() << std::endl;
